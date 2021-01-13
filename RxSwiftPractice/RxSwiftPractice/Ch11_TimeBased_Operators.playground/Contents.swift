6380dfa2a9b0b46da5c727dad3ef2ed821ec0f7b
조상호 <97chos@naver.com>
2021-01-14 00:37:14 +0900

    project Files

--- RxSwiftPractice/RxSwiftPractice/Ch11_TimeBased_Operators.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice/Ch11_TimeBased_Operators.playground/Contents.swift
@@ -6,13 +6,12 @@ import RxSwift
 do {
   // MARK: - replay(_:), replayAll()
 
-  let elementsPersecond = 1
+  let elementsPerSecond = 1
   let maxElements = 5
   let replayedElements = 1
   let replayDelay:TimeInterval = 3
 
   let sourceObservable = Observable<Int>.create { observer in
     var value = 1
-    let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPersecond))
-  }
+    }
 }
