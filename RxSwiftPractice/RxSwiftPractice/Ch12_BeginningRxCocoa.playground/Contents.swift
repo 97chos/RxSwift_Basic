f63677231296985786568b3c0e2ecdf8d1305264
조상호 <97chos@naver.com>
2021-01-16 17:44:45 +0900

    new feature 2-2

--- RxSwiftPractice/RxSwiftPractice/Ch12_BeginningRxCocoa.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice/Ch12_BeginningRxCocoa.playground/Contents.swift
@@ -4,3 +4,5 @@ import UIKit
 var str = "master commit 3"
 var feature1 = "Add new feature 1-2"
 var feature = "Add new feature 2"
+var feature = "Add new feature 2-2"
+>>>>>>> 7875ae6... new feature 2-2
